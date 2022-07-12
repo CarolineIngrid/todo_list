a536aec68fea3a757bb04948d67b7f7f6958f282
Gabriela Victoria <gabrielavictoria91@gmail.com>
2022-07-12 15:10:49 -0300

    Tarefas aparecendo no HTML

--- lista/models.py
+++ lista/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Tarefa(models.Model):
+    # transformar em cores
+    OPCAO_CATEGORIA = (
+        ('urgente', 'Urgente'),
+        ('importante', 'Importante'),
+        ('precisa ser feito', 'Precisa ser feito'),
+    )
+
+
+    descricao = models.CharField(max_length=400)
+    criacao = models.DateTimeField(auto_now_add=True)
+    categoria = models.CharField(max_length=25, choices=OPCAO_CATEGORIA,
+                                 default='importante')
+    status = models.BooleanField(default=False)
+
