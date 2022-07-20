5c345e2ab01e4d3c73daab6bf17b24f7519ee724
Gabriela Victoria <gabrielavictoria91@gmail.com>
2022-07-20 14:09:46 -0300

    iniando estilo no forms

--- lista/forms.py
+++ lista/forms.py
@@ -6,10 +6,21 @@ from .models import Tarefa
 class ListaForms(forms.ModelForm):
     class Meta:
         model = Tarefa
-        fields = [
-            "titulo",
-            "descricao",
-            "categoria",
-            "status",
+        fields = (
+            'titulo',
+            'descricao',
+            'categoria',
+            'status',
+        )
 
-        ]
+        widget = {
+            'titulo': forms.TextInput(attrs={'class': 'form-control',
+                                             'placeholder': 'Adicione um nome a tarefa. Ex: Arrumar o quarto.'}),
+            'descricao': forms.TextInput(attrs={'class':
+                                                    'form-control',
+                                                'placeholder':
+                                                    'Deixe-a mais específica. Ex: Deixar tudo organizado. '
+                                                    'Principalmente embaixo da cama!'}),
+            'categoria': forms.Select(attrs={'class': 'form-control'}),
+            'status': forms.Textarea(attrs={'class': 'form-control'}),
+        }
--- lista/models.py
+++ lista/models.py
@@ -10,8 +10,8 @@ class Tarefa(models.Model):
         ('a fazer', 'A fazer'),
     )
 
-    titulo = models.CharField(default='', max_length=24, blank=True)
-    descricao = models.CharField(max_length=300)
+    titulo = models.CharField(default='', max_length=255, )
+    descricao = models.TextField(blank=True, null=True)
     criacao = models.DateTimeField(auto_now_add=True)
     categoria = models.CharField(max_length=25, choices=OPCAO_CATEGORIA, default='importante')
     status = models.BooleanField(default=False)
