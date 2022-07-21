4177ebebd42d18ca24c373ec9e9b3273ff60bc19
Gabriela Victoria <gabrielavictoria91@gmail.com>
2022-07-21 15:02:11 -0300

    template de adicionar tarefa com style

--- lista/forms.py
+++ lista/forms.py
@@ -1,9 +1,15 @@
-
 from django import forms
 
 from .models import Tarefa
 
+
 class ListaForms(forms.ModelForm):
+
+    titulo = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Ex: Arrumar o quarto."}))
+    descricao = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control", "placeholder": "Deixe a tarefa mais específica. Ex: Deixar tudo organizado. Principalmente embaixo da cama!"}))
+    # categoria = forms.Select(attrs={'class': 'form-control'}),
+    # status = forms.Textarea(attrs={'class': 'form-control'}),
+
     class Meta:
         model = Tarefa
         fields = (
@@ -13,14 +19,14 @@ class ListaForms(forms.ModelForm):
             'status',
         )
 
-        widget = {
-            'titulo': forms.TextInput(attrs={'class': 'form-control',
-                                             'placeholder': 'Adicione um nome a tarefa. Ex: Arrumar o quarto.'}),
-            'descricao': forms.TextInput(attrs={'class':
-                                                    'form-control',
-                                                'placeholder':
-                                                    'Deixe-a mais específica. Ex: Deixar tudo organizado. '
-                                                    'Principalmente embaixo da cama!'}),
-            'categoria': forms.Select(attrs={'class': 'form-control'}),
-            'status': forms.Textarea(attrs={'class': 'form-control'}),
-        }
+        # widget = {
+        #     'titulo': forms.TextInput(attrs={'class': 'form-control',
+        #                                      'placeholder': 'Adicione um nome a tarefa. Ex: Arrumar o quarto.'}),
+        #     'descricao': forms.TextInput(attrs={'class':
+        #                                             'form-control',
+        #                                         'placeholder':
+        #                                             'Deixe-a mais específica. Ex: Deixar tudo organizado. '
+        #                                             'Principalmente embaixo da cama!'}),
+        #     'categoria': forms.Select(attrs={'class': 'form-control'}),
+        #     'status': forms.Textarea(attrs={'class': 'form-control'}),
+        # }
--- lista/models.py
+++ lista/models.py
@@ -5,15 +5,16 @@ from django.db import models
 class Tarefa(models.Model):
     # transformar em cores
     OPCAO_CATEGORIA = (
+        (None, '---------'),
         ('urgente', 'Urgente'),
         ('moderada', 'Moderada'),
         ('a fazer', 'A fazer'),
     )
 
     titulo = models.CharField(default='', max_length=255, )
-    descricao = models.TextField(blank=True, null=True)
+    descricao = models.TextField(blank=True)
     criacao = models.DateTimeField(auto_now_add=True)
-    categoria = models.CharField(max_length=25, choices=OPCAO_CATEGORIA, default='importante')
+    categoria = models.CharField(max_length=25, choices=OPCAO_CATEGORIA, default='---------')
     status = models.BooleanField(default=False)
 
     def __str__(self):
