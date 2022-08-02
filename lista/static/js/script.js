4cd69a66f6e20c5d35304ac193d492d373780b87
Ingrid Caroline <ingridcaroline3012@gmail.com>
2022-08-02 14:25:57 -0300

    lógica ok, menu

--- lista/static/js/script.js
+++ lista/static/js/script.js
@@ -1,3 +1,8 @@
-function myFunction() {
-    document.getElementById("demo").style.color = "red";
-  }
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(){
+  const nav = document.querySelector('#nav');
+  nav.classList.add('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
